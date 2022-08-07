c284670f11ea37053d7b76d0004ba108d0e6a27b
mohammad taha abdollah <tahaabdollah2002@gmail.com>
2022-08-07 14:11:39 +0430

    update form for add tasks

--- src/TopForm.jsx
+++ src/TopForm.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import { taskContext } from './taskContext';
 
 const TopForm = ()=>{
+    const [task, setTask] = useState('');
+    const {taskItems, setTaskItems} = useContext(taskContext);
+    const handleSetTask = (event) =>{
+        setTask(event.target.value)
+    }
+    const handleAddTask = (event) =>{
+        event.preventDefault()
+        setTaskItems([...taskItems, {id: Math.random, title: task, done: false}])
+    }
     return(
         <React.Fragment>
             <h4 className="text-center text-info text_shdow">To-Do Application</h4>
-            <form>
+            <form onSubmit={handleAddTask}>
                 <div className="form-group d-flex">
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" value={task} onChange={handleSetTask} />
                     <button type="submit" className="btn btn-success me-1">ثبت</button>
                 </div>
             </form>
