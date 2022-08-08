747b8cf998a40fa1908378982a43980793ec9e4f
mohammad taha abdollah <tahaabdollah2002@gmail.com>
2022-08-09 01:01:17 +0430

    use useRef for focused input after mount

--- src/TopForm.jsx
+++ src/TopForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext, useEffect, useRef} from 'react';
 import { taskContext } from './taskContext';
 
 const TopForm = ()=>{
@@ -23,12 +23,17 @@ const TopForm = ()=>{
                 setTask('')
         }
     }
+    const inputRef = useRef()
+    useEffect(() => {
+        inputRef.current.focus()
+        return () => {};
+    }, []);
     return(
         <React.Fragment>
             <h4 className="text-center text-info text_shdow">To-Do Application</h4>
             <form onSubmit={handleAddTask}>
                 <div className="form-group d-flex">
-                    <input type="text" className="form-control me-1" value={task} onChange={handleSetTask} />
+                    <input type="text" className="form-control me-1" value={task} ref={inputRef} onChange={handleSetTask} />
                     <button type="submit" className="btn btn-success">Add</button>
                 </div>
                     {task.length<5 ? <p className='form-text text-danger ms-2 mt-1'>Please inter at least 5 characters.</p> : ''}
